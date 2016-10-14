a62ddb56a60262648c28eaff7946f8bcb01ee8bb
WeiFoo <wfu@ncsu.edu>
2016-10-14 17:13:27 -0400

    update

--- project/authentication.py
+++ project/authentication.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 
+
 def login(user_name, pass_word):
   try:
     user_file = open("users.txt")
     user_buf = user_file.read()
-    users = [ line.split("|") for line in user_buf.split("\n")]
+    users = [line.split("|") for line in user_buf.split("\n")]
     if [user_name, pass_word] in users:
       return True
     else:
@@ -13,7 +14,7 @@ def login(user_name, pass_word):
     print("I can't autheticate you.")
     return False
 
+
 def logout():
   print("This line will not be tested!")
   print("This line also won't be teseted!")
-
--- tests/authentication_tests.py
+++ tests/authentication_tests.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from mock import patch
 import project.authentication as auth
 
+
 class StandAloneTests(TestCase):
   """Teset the stand-alone module functions."""
 
@@ -14,22 +15,19 @@ class StandAloneTests(TestCase):
     :return:
     """
     mock_open.return_value.read.return_value = "ncsu|cs"
-    self.assertTrue(auth.login('ncsu','cs'))
+    self.assertTrue(auth.login('ncsu', 'cs'))
 
   @patch('__builtin__.open')
-  def test_login_failed(self,mock_open):
+  def test_login_failed(self, mock_open):
     mock_open.return_value.read.return_value = "ncsu|cs"
-    self.assertTrue(auth.login('ncsu','ece'))
+    self.assertTrue(auth.login('ncsu', 'ece'))
 
   @patch('__builtin__.open')
-  def test_login_error(self,mock_open):
+  def test_login_error(self, mock_open):
     """
     I/O error
     :param mock_open:
     :return:
     """
     mock_open.side_effect = IOError()
-    self.assertTrue(auth.login('ncsu','cs'))
-
-
-
+    self.assertTrue(auth.login('ncsu', 'cs'))
