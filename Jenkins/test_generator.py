8b6e474699ea2a130c639a7e624663bc16318144
WeiFoo <wfu@ncsu.edu>
2016-10-17 23:01:41 -0400

    add

--- Jenkins/test_generator.py
+++ Jenkins/test_generator.py
@@ -5,15 +5,6 @@ import pdb
 import random
 from httpbin import core
 
-# file_to_parse = "saws.py"
-# with open(file_to_parse) as fd:
-#   file_contents = fd.read()
-#
-# module = ast.parse(file_contents)
-# class_definitions = [node for node in module.body if
-#                      isinstance(node, ast.ClassDef)]
-# function_definitions = [node for node in class_definitions[0].body if
-#                         isinstance(node, ast.FunctionDef)]
 constraints = {}
 max_condition = {}
 long_methods = {}
@@ -96,6 +87,7 @@ class FuncLister(ast.NodeVisitor):
           continue
         if one.func.value.id == "app" and one.func.attr == "route":
           constraints[node.name] = one.args[0].s
+    pdb.set_trace()
     max_condition[node.name] = self.calculate_max_condition(node)
     long_methods[node.name] = node.body[-1].lineno - node.lineno + 1
     self.generic_visit(node)
@@ -121,18 +113,6 @@ class FuncLister(ast.NodeVisitor):
 
 
 
-
-
-
-
-      # def visit_If(self, node):
-      #   pdb.set_trace()
-      #   if hasattr(node.test,"values"):
-      #     print("max_conditions",len(node.test.values))
-      #   else:
-      #     print("max_conditions",1)
-      #
-      #   self.generic_visit(node)
 
 
 if __name__ == "__main__":
